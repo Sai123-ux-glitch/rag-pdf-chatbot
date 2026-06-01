0f09a1d0d30f4a955ef51e5de2246c72637d7495
sai vardhan <saivardhan@sais-MacBook-Air-2.local>
2026-06-01 23:51:06 +0530

    Fix: update imports for newer langchain packages

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import streamlit as st
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain.chains import RetrievalQA
 
